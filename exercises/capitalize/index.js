aa5a6db99d387882f41ba7efc7f2dad0bd100b73
Languiller <Daniel.Languiller@team.telstra.com>
2019-09-05 10:12:06 +1000

    finished steps v1

--- exercises/capitalize/index.js
+++ exercises/capitalize/index.js
@@ -7,6 +7,41 @@
 //   capitalize('a lazy fox') --> 'A Lazy Fox'
 //   capitalize('look, it is working!') --> 'Look, It Is Working!'
 
-function capitalize(str) {}
+function capitalize(str) {
+  //Solution #1
+  //   let newStr = str.split(" ");
+  //   let returner = [];
+
+  //   for (let word of newStr) {
+  //     returner.push(word[0].toUpperCase() + word.slice(1));
+  //   }
+  //   returner = returner.join(" ");
+  //   return returner;
+  //************************ */
+  // Solution #2
+  //   const words = [];
+  //   for (let word of str.split(" ")) {
+  //     words.push(word[0].toUpperCase() + word.slice(1));
+  //   }
+
+  //   return words.join(" ");
+  //************************** */
+  //Solution #3
+  let result = str[0].toUpperCase();
+
+  for (let i = 1; i < str.length; i++) {
+    if (str[i - 1] === " ") {
+      result += str[i].toUpperCase();
+    } else {
+      result += str[i];
+    }
+  }
+
+  return result;
+}
+
+capitalize("a short sentence"); // --> 'A Short Sentence'
+capitalize("a lazy fox"); // --> 'A Lazy Fox'
+capitalize("look, it is working!"); // --> 'Look, It Is Working!'
 
 module.exports = capitalize;
