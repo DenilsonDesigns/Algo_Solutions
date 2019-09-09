f1b9e0d228b3a606d6fdeefce1312c8235a60847
Languiller <Daniel.Languiller@team.telstra.com>
2019-09-09 10:31:36 +1000

    finished matrix spiral

--- exercises/vowels/index.js
+++ exercises/vowels/index.js
@@ -7,6 +7,37 @@
 //   vowels('Why do you ask?') --> 4
 //   vowels('Why?') --> 0
 
-function vowels(str) {}
+function vowels(str) {
+  // Solution #!
+  //   let vowels = "AEIOU";
+  //   let count = 0;
+
+  //   for (let char of str) {
+  //     if (vowels.includes(char.toUpperCase())) {
+  //       console.log(char);
+  //       count++;
+  //     }
+  //   }
+  //   return count;
+  //*********************** */
+  // Solution #2
+  // let count = 0;
+  // const checker = ["a", "e", "i", "o", "u"];
+
+  // for (let char of str.toLowerCase()) {
+  //   if (checker.includes(char)) {
+  //     count++;
+  //   }
+  // }
+  // return count;
+  //************************** */
+  // Solution #3
+  const matches = str.match(/[aeiou]/gi);
+  return matches ? matches.length : 0;
+}
+
+vowels("Hi There!"); // --> 3
+//   vowels('Why do you ask?');// --> 4
+//   vowels('Why?');// --> 0
 
 module.exports = vowels;
