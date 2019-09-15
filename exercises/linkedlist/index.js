e1bf87582ff293827e6c73168b18743a8da64d10
Languiller <Daniel.Languiller@team.telstra.com>
2019-09-16 09:58:04 +1000

    finished getAt

--- exercises/linkedlist/index.js
+++ exercises/linkedlist/index.js
@@ -58,6 +58,51 @@ class LinkedList {
 
     this.head = this.head.next;
   }
+
+  removeLast() {
+    if (!this.head) {
+      return;
+    }
+
+    if (!this.head.next) {
+      this.head = null;
+      return;
+    }
+
+    let previous = this.head;
+    let node = this.head.next;
+
+    while (node.next) {
+      previous = node;
+      node = node.next;
+    }
+    previous.next = null;
+  }
+
+  insertLast(data) {
+    const last = this.getLast();
+
+    if (last) {
+      // There are some existing nodes in our chain
+      last.next = new Node(data);
+    } else {
+      // The chain is empty!
+      this.head = new Node(data);
+    }
+  }
+
+  getAt(index) {
+    let counter = 0;
+    let node = this.head;
+    while (node) {
+      if (counter === index) {
+        return node;
+      }
+      counter++;
+      node = node.next;
+    }
+    return null;
+  }
 }
 
 module.exports = { Node, LinkedList };
